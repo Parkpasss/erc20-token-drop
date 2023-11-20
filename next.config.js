7a8a248b5172367bb27877e7a96c370067675601
Park Jina <parkpass_@naver.com>
2023-11-20 11:46:54 +0900

    first commit

--- next.config.js
+++ next.config.js
@@ -2,8 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["gateway.ipfscdn.io"],
+    domains: [
+      'gateway.ipfscdn.io',
+      'bafybeifmmo646ymfr2dstvl6vehwhmqucgqbszj6x4fkmqnqjy4ccfcfn4.ipfs.cf-ipfs.com',
+      '389f71992934141cbe4be69d37e078a8.ipfscdn.io',
+    ],
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
